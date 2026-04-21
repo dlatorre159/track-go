1405ae9e64f81dae53eac97bdd62d98fbe747dab
Magklo <a20200798@pucp.edu.pe>
2026-04-21 16:28:03 -0500

    dbManager

--- Track-Go-DB-Manager/src/main/java/DBManager.java
+++ Track-Go-DB-Manager/src/main/java/DBManager.java
@@ -1,28 +1,61 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class DBManager{
+    private static DBManager instance;
+    private static Connection connection;
+    private String host;
+    private int port;
+    private String esquema;
+    private String usuario;
+    private String password;
 
 
-public abstract class DBManager {
-    protected String host;
-    protected int puerto;
-    protected String esquema;
-    protected String usuario;
-    protected String password;
+    private DBManager(){
+        ResourceBundle db = ResourceBundle.getBundle("db");
+        this.host = db.getString("db.host");
+        this.port = Integer.parseInt(db.getString("db.puerto"));
+        this.esquema = db.getString("db.esquema");
+        this.usuario = db.getString("db.usuario");
+        this.password = db.getString("db.password");
 
-    protected DBManager() {}
+        String url = "jdbc:mysql://" + this.host + ":" + this.port + "/" + this.esquema;
 
-    protected DBManager(String host, int puerto, String esquema, String password, String usuario){
         try{
-            this.host = host;
-            this.puerto = puerto;
-            this.esquema = esquema;
-            this.usuario = usuario;
-            this.password = password;
-        } catch (RuntimeException e) {
-            System.out.println("Error al conectar a la base de datos ");
-            throw new RuntimeException(e);
+            connection = DriverManager.getConnection(url,this.usuario,this.password);
+            if(connection != null && !connection.isClosed()){
+                System.out.println("Conexión exitosa a la Base de Datos");
+            }
+        }catch (SQLException e){
+            System.out.println("Error al conectar con la Base de Datos");
         }
     }
 
-    public abstract Connection getConnection() throws SQLException, ClassNotFoundException;
+    public static DBManager getInstance(){
+        if(instance == null){
+            synchronized (DBManager.class){
+                if(instance == null){
+                    instance = new DBManager();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static Connection getConnection(){
+        return connection;
+    }
+
+    public static void closeConnection(){
+        try{
+            if(connection != null && !connection.isClosed()){
+                connection.close();
+                System.out.println("Conexión cerrada");
+            }
+        }catch(SQLException e){
+            System.out.println("Error al cerrar la conexión");
+        }
+    }
 }
